686cb40ab196219a2d5f9a94e14a2eb9a6c5fee0
Andreas Pankratz <andreas.pankratz@s-square.de>
2009-04-29 21:10:38 +0000

    Added keywords

--- library/ZFDebug/Controller/Plugin/Debug/Plugin/Variables.php
+++ library/ZFDebug/Controller/Plugin/Debug/Plugin/Variables.php
@@ -7,7 +7,7 @@
  * @subpackage Plugins
  * @copyright  Copyright (c) 2008-2009 ZF Debug Bar Team (http://code.google.com/p/zfdebug)
  * @license    http://code.google.com/p/zfdebug/wiki/License     New BSD License
- * @version    $Id: $
+ * @version    $Id$
  */
 
 /**
