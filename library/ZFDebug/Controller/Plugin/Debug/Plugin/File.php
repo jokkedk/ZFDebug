a0338171e914e76af5e08e59b6bc4ed1ef127b5a
Andreas Pankratz <andreas.pankratz@s-square.de>
2009-05-01 13:57:26 +0000

    - Added Support for own library - Changed some code to be compliant with ZF Coding Standards

--- library/ZFDebug/Controller/Plugin/Debug/Plugin/File.php
+++ library/ZFDebug/Controller/Plugin/Debug/Plugin/File.php
@@ -33,21 +33,42 @@ class ZFDebug_Controller_Plugin_Debug_Plugin_File implements ZFDebug_Controller_
      * @var string
      */
     protected $_basePath;
-    
-    protected $_includedFiles = null;
-
 
     /**
-     * Setting Base Path
+     * Stores included files
+     *
+     * @var array
+     */
+    protected $_includedFiles = null;
+
+    /**
+     * Stores name of own extension library
+     *
+     * @var string
+     */
+    protected $_myLibrary;
+
+    /**
+     * Setting Options
+     *
+     * basepath:
+     * This will normally not your document root of your webserver, its your
+     * application root directory with /application, /library and /public
+     *
+     * myLibrary:
+     * Your own library extension
      *
      * @param string $basePath
-     * @revurn void
+     * @param
+     * @return void
      */
-    public function __construct($basePath = '')
+    public function __construct($basePath = '', $myLibary = null)
     {
-        if ($basePath == '')
+        if ($basePath == '') {
             $basePath = $_SERVER['DOCUMENT_ROOT'];
+        }
         $this->_basePath = $basePath;
+        $this->_myLibrary = $myLibary;
     }
 
     /**
@@ -67,7 +88,7 @@ class ZFDebug_Controller_Plugin_Debug_Plugin_File implements ZFDebug_Controller_
      */
     public function getTab()
     {
-        return 'Files (' . count($this->getIncludedFiles()) . ')';
+        return 'Files (' . count($this->_getIncludedFiles()) . ')';
     }
 
     /**
@@ -77,33 +98,55 @@ class ZFDebug_Controller_Plugin_Debug_Plugin_File implements ZFDebug_Controller_
      */
     public function getPanel()
     {
-        $included = $this->getIncludedFiles();
+        $included = $this->_getIncludedFiles();
         $html = '<h4>File Information</h4>';
         $html .= 'Total Files included: ' . count($included) . '<br />';
         $html .= 'Basepath: ' . $this->_basePath . '<br />';
 
         $frameworkFiles = '<h4>Zend Framework Files</h4>';
         $zfdebugFiles = '<h4>ZFDebug Files</h4>';
+        $myFiles = '<h4>' . $this->_myLibrary . ' Files</h4>';
 
         $html .= '<h4>Application Files</h4>';
         foreach ($included as $file) {
             $file = str_replace($this->_basePath, '', $file);
-            if (false !== strstr($file, 'Zend'))
-                $frameworkFiles .= $file.'<br>';
-            elseif (false !== strstr($file, 'ZFDebug'))
-                $zfdebugFiles .= $file.'<br>';
-            else
-                $html .= $file.'<br>';
+            if (false !== strstr($file, 'Zend')) {
+                $frameworkFiles .= $file . '<br />';
+            } elseif (false !== strstr($file, 'ZFDebug')) {
+                $zfdebugFiles .= $file . '<br />';
+            } else {
+            	if (null !== $this->_myLibrary)
+            	{
+            		if(false !== strstr($file, $this->_myLibrary)) {
+            			$myFiles .= $file . '<br />';
+            		} else {
+            			$html .= $file . '<br />';
+            		}
+            	} else {
+                $html .= $file . '<br />';
+            	}
+            }
         }
-        $html .= $frameworkFiles . $zfdebugFiles;
+
+        if(null !== $this->_myLibrary) {
+        	$html .= $myFiles;
+        }
+
+        $html .=  $zfdebugFiles . $frameworkFiles;
         return $html;
     }
-    
-    protected function getIncludedFiles()
+
+    /**
+     * Gets included files
+     *
+     * @return array
+     */
+    protected function _getIncludedFiles()
     {
-        if (null !== $this->_includedFiles)
+        if (null !== $this->_includedFiles) {
             return $this->_includedFiles;
-            
+        }
+
         $this->_includedFiles = get_included_files();
         sort($this->_includedFiles);
         return $this->_includedFiles;
