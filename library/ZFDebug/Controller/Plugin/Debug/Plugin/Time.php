5803f4a927bd44a5fbfe64b3762d5d3525843f61
Andreas Pankratz <andreas.pankratz@s-square.de>
2009-05-01 15:52:09 +0000

    Fixed Bug throwing exception if time plugin was wrongfully loaded two times

--- library/ZFDebug/Controller/Plugin/Debug/Plugin/Time.php
+++ library/ZFDebug/Controller/Plugin/Debug/Plugin/Time.php
@@ -96,7 +96,11 @@ class ZFDebug_Controller_Plugin_Debug_Plugin_Time extends Zend_Controller_Plugin
         $action = $request->getActionName();
 
 
-        $timerNamespace = new Zend_Session_Namespace('ZFDebug_Time',true);
+        if (Zend_Session::namespaceIsset('ZFDebug_Time')) {
+        	$timerNamespace = Zend_Session::namespaceGet('ZFDebug_Time');
+        } else {
+        	$timerNamespace = new Zend_Session_Namespace('ZFDebug_Time',true);
+        }
         $timerNamespace->data[$module][$controller][$action][] = $this->_timer['postDispatch'];
 
         $html .= '<h4>Overall Timers</h4>';
