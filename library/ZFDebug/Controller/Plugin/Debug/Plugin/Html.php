6f17b0b85b9c08cccd03a4de942c00e101c98ae8
Joakim Nygård <joakim@jokke.dk>
2009-05-14 10:12:52 +0000

    - Adding simple Html plugin

---
+++ library/ZFDebug/Controller/Plugin/Debug/Plugin/Html.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * ZFDebug Zend Additions
+ *
+ * @category   ZFDebug
+ * @package    ZFDebug_Controller
+ * @subpackage Plugins
+ * @copyright  Copyright (c) 2008-2009 ZF Debug Bar Team (http://code.google.com/p/zfdebug)
+ * @license    http://code.google.com/p/zfdebug/wiki/License     New BSD License
+ * @version    $Id$
+ */
+
+/**
+ * @category   ZFDebug
+ * @package    ZFDebug_Controller
+ * @subpackage Plugins
+ * @copyright  Copyright (c) 2008-2009 ZF Debug Bar Team (http://code.google.com/p/zfdebug)
+ * @license    http://code.google.com/p/zfdebug/wiki/License     New BSD License
+ */
+class ZFDebug_Controller_Plugin_Debug_Plugin_Html implements ZFDebug_Controller_Plugin_Debug_Plugin_Interface
+{
+    /**
+     * Contains plugin identifier name
+     *
+     * @var string
+     */
+    protected $_identifier = 'html';
+
+    /**
+     * Create ZFDebug_Controller_Plugin_Debug_Plugin_Html
+     *
+     * @param string $tab
+     * @paran string $panel
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Gets identifier for this plugin
+     *
+     * @return string
+     */
+    public function getIdentifier()
+    {
+        return $this->_identifier;
+    }
+
+    /**
+     * Gets menu tab for the Debugbar
+     *
+     * @return string
+     */
+    public function getTab()
+    {
+        return 'HTML';
+    }
+
+    /**
+     * Gets content panel for the Debugbar
+     *
+     * @return string
+     */
+    public function getPanel()
+    {
+        $body = Zend_Controller_Front::getInstance()->getResponse()->getBody();
+        $panel = '<h4>HTML Information</h4>';
+        $panel .= '
+        <script type="text/javascript" charset="utf-8">
+            var load = window.onload;
+            window.onload = function(){
+                if (load) {
+                    load();
+                }
+                jQuery("#ZFDebug_Html_Tagcount").html(document.getElementsByTagName("*").length);
+                jQuery("#ZFDebug_Html_Stylecount").html(jQuery("link[rel*=stylesheet]").length);
+                jQuery("#ZFDebug_Html_Scriptcount").html(jQuery("script[src]").length);
+                jQuery("#ZFDebug_Html_Imgcount").html(jQuery("img[src]").length);
+            };
+            load = "";
+        </script>';
+        $panel .= '<span id="ZFDebug_Html_Tagcount"></span> Tags<br />'
+                . 'HTML Size: '.round(strlen($body)/1024, 2).'K<br />'
+                . '<span id="ZFDebug_Html_Stylecount"></span> Stylesheet File<br />'
+                . '<span id="ZFDebug_Html_Scriptcount"></span> Javascript File<br />'
+                . '<span id="ZFDebug_Html_Imgcount"></span> Images<br />'
+                . '<form method="POST" action="http://validator.w3.org/check" target="_blank"><input type="hidden" name="fragment" value="'.htmlentities($body).'"><input type="submit" value="Validate With W3"></form>';
+        return $panel;
+    }
+}
