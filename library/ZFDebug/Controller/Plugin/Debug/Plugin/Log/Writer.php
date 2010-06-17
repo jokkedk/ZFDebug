0046ededcd1d32f05bb78eed50c020cd0befabde
Joakim Nygård <joakim@jokke.dk>
2010-06-17 22:33:08 +0000

    [CHANGED] Log output colouring

--- library/ZFDebug/Controller/Plugin/Debug/Plugin/Log/Writer.php
+++ library/ZFDebug/Controller/Plugin/Debug/Plugin/Log/Writer.php
@@ -46,10 +46,10 @@ class ZFDebug_Controller_Plugin_Debug_Plugin_Log_Writer extends Zend_Log_Writer_
     protected function _write($event)
     {
         // $output = '<table cellspacing="10">';
-        $output = '<tr style="color:%color%;">';
-        $output .= '<td style="text-align:right;padding-right:1em">%priorityName%</td>';
-        $output .= '<td style="text-align:right;padding-right:1em">%memory%</td>';
-        $output .= '<td style="width:90%;">%message%</td></tr>'; // (%priority%)
+        $output = '<tr>';
+        $output .= '<td style="color:%color%;text-align:right;padding-right:1em">%priorityName%</td>';
+        $output .= '<td style="color:%color%;text-align:right;padding-right:1em">%memory%</td>';
+        $output .= '<td style="color:%color%;">%message%</td></tr>'; // (%priority%)
         $event['color'] = '#C9C9C9';
         // Count errors
         if ($event['priority'] < 7) {
