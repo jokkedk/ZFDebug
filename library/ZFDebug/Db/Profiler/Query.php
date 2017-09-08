5b15fceb703d9a1259d57483c76b042bf693c29d
Joakim Nygård <joakim@scienta.dk>
2017-09-08 12:34:17 +0200

    Add doc block

--- library/ZFDebug/Db/Profiler/Query.php
+++ library/ZFDebug/Db/Profiler/Query.php
@@ -1,5 +1,22 @@
 <?php
+/**
+ * ZFDebug Zend Additions
+ *
+ * @category   ZFDebug
+ * @package    ZFDebug_Db
+ * @subpackage Profiler
+ * @author     Joakim Nygård
+ * @license    http://github.com/jokkedk/ZFDebug/blob/master/license     New BSD License
+ * @version    $Id$
+ */
 
+ /**
+ * @category   ZFDebug
+ * @package    ZFDebug_Db
+ * @subpackage Profiler
+ * @author     Joakim Nygård
+ * @license    http://github.com/jokkedk/ZFDebug/blob/master/license     New BSD License
+ */
 class ZFDebug_Db_Profiler_Query extends Zend_Db_Profiler_Query
 {
     protected $_trace = [];
